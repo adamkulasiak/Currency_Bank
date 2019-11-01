ad1819417b65291c422abeff5ac276f46a26af35
adam.kulasiak <adam.kulasiak@gmail.com>
2019-11-01 13:09:07 +0100

    Created logic layer project

---
+++ CurrencyBank.BLL/Interfaces/IAuth.cs
@@ -0,0 +1,12 @@
+﻿using Database.Models;
+using System.Threading.Tasks;
+
+namespace CurrencyBank.BLL.Interfaces
+{
+    interface IAuth
+    {
+        Task<User> Login(string username, string password);
+        Task<User> Register(User user, string password);
+        Task<bool> UserExists(string username);
+    }
+}
