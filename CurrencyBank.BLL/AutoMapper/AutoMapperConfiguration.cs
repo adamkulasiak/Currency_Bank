fb5a624690db10ed8b6ec0bacd1c24b27238bc21
adam.kulasiak <adam.kulasiak@gmail.com>
2019-11-05 10:54:30 +0100

    some wpf issues fixed

---
+++ CurrencyBank.BLL/AutoMapper/AutoMapperConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CurrencyBank.BLL.Dtos;
+using Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurrencyBank.BLL.AutoMapper
+{
+    public class AutoMapperConfiguration
+    {
+        public AutoMapperConfiguration()
+        {
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<User, UserRegisterDto>();
+            });
+        }
+         
+    }
+}
